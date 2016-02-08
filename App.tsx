2efc26a0ff788bf7fd944b0c72a025c385366d4a
Danny Libin <omegasol11@gmail.com>
2016-02-08 17:36:39 -0600

    starting design skeleton

--- App.tsx
+++ App.tsx
@@ -2,7 +2,41 @@ import * as React from 'react';
 
 class App extends React.Component<any, any> {
 	render() {
-		return <div>Hello Alice</div>
+		return (
+			<div id="page-wrapper">
+				<div id="title"><span>Recipe Box</span></div>
+				<RecipeBox />
+			</div>
+		);
+	}
+}
+
+class RecipeBox extends React.Component<any, any> {
+	render() {
+		return (
+			<div id="recipe-box" className="pure-g">
+				<RecipeList />
+				<RecipeDetails />
+			</div>
+		);
+	}
+}
+
+class RecipeList extends React.Component<any, any> {
+	render() {
+		return (
+			<div id="recipe-list" className="pure-u-1 pure-u-xl-1-4">
+			</div>
+		);
+	}
+}
+
+class RecipeDetails extends React.Component<any, any> {
+	render() {
+		return (
+			<div id="recipe-details" className="pure-u-1 pure-u-xl-3-4">
+			</div>
+		);
 	}
 }
 
